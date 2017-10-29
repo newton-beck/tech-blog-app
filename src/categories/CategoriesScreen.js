038fe6719ddcea672f449f3784f7188b83244930
“Newton <newton.beck@gmail.com>
2017-10-29 10:54:24 -0200

    Altera a tela de categorias para buscar dados do backend

--- src/categories/CategoriesScreen.js
+++ src/categories/CategoriesScreen.js
@@ -12,28 +12,27 @@ export default class CategoriesScreen extends Component {
     headerTintColor: '#fff'
   });
 
+  constructor() {
+    super();
+    this.state = { posts: [] };
+  }
+
+  componentDidMount() {
+    let { params } = this.props.navigation.state;
+    fetch('http://192.168.25.188:3000/posts/' + params.name)
+    .then((resp) => resp.json())
+    .then((posts) => {
+      this.setState({ posts: posts });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  }
+
   render() {
-    let posts = [
-      {
-        key: 3,
-        title: 'Técnicas para codificar um e-mail',
-        author: '@erikatakahara',
-        date: '09/10/2017',
-        description: 'Todo desenvolvedor front-end já passou por problemas ao codificar um e-mail, principalmente quando se trata de compatibilidade com todos os clientes, como o Yahoo, Gmail, Outlook, etc. Neste post, iremos abordar algumas técnicas e quais são os principais pontos de atenção ao desenvolver esse tipo de layout.',
-        category: 'front-end'
-      },
-      {
-        key: 4,
-        title: 'Customizando campos de formulário com o mask-amd',
-        author: '@rapahaeru',
-        date: '25/09/2017',
-        description: 'Que tal otimizar seu tempo e seu HTML com uma biblioteca focada diretamente no que é preciso? Direto ao ponto, a mask-amd tem um único objetivo: formatar os campos do seu formulário.',
-        category: 'front-end'
-      }
-    ];
     return (
       <View style={styles.container}>
-        <Posts posts={posts} goToCategory={({ category }) => {}} />
+        <Posts posts={this.state.posts} goToCategory={({ category }) => {}} />
       </View>
     );
   }
